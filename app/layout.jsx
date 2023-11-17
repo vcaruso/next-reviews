9f4e92c885225a32c95c576d9614b1674a1bec84
vincenzocaruso <enzo@carusovincenzo.com>
2023-11-17 17:30:52 +0100

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          [header]
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          [footer]
+        </footer>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <>
+        <h1>Indie gamer</h1>
+        <p>
+            Only the best indie games, reviewd for you.
+        </p>
+    </>
+   
+  )
+}
