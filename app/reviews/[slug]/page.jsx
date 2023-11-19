2a8fedd3e0fe3e773fe64526d7cef11306f82967
vcaruso <vcaruso@xeniaprogetti.it>
2023-11-19 12:09:15 +0100

    Client Component 8

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Heading from '@/components/Heading';
 import { getReview, getSlugs } from '@/lib/reviews';
+import ShareLinkButton from '@/components/ShareLinkButton';
+import ShareButtons from '@/components/ShareButtons';
 
 export async function generateStaticParams(){
     const slugs = await getSlugs();  
@@ -24,7 +26,11 @@ export default async function ReviewPage({params:{slug}}) {
   return (
     <>
         <Heading>{review.title}</Heading>
-        <p className='italic pb-2'>{review.date}</p>
+        <div className='flex gap-3 items-baseline'>
+          <p className='italic pb-2'>{review.date}</p>
+          <ShareLinkButton />
+        </div>
+        
         <img src={review.image} width={640} height={360} className='mb-2 rounded' />
         <article dangerouslySetInnerHTML={{__html:review.body}}
           className='max-w-screen-sm prose prose-slate'
---
+++ components/ShareButtons.jsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react'
+import ShareLinkButton from './ShareLinkButton'
+
+export default function ShareButtons() {
+  return (
+    <div>
+        <ShareLinkButton /> | [Twitter] | [Facebook]
+    </div>
+  )
+}
